21c7a1027b8a4356e6c23160bc446a6c5a9dea9a
samirastk <samira.khan@ucalgary.ca>
2025-07-13 22:10:01 -0600

    first try

--- mwb-site/src/App.tsx
+++ mwb-site/src/App.tsx
@@ -1,29 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { NotFound } from './pages/NotFound'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+      <Routes>
+        <Route index element ={<Home/>}/>
+        <Route path="/NotFound" element={<NotFound/>} />
+        <Route path="*" element={<Navigate to="/NotFound" replace />} />
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ mwb-site/src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+export const NotFound = () => {
+    return(
+        <div>
+            ERROR 404 or whatever
+        </div>
+    )
+
+
+}
